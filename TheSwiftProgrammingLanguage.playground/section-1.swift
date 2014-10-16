74d5a1cf446c783b619858cb3af73049c96af90e
Dongwoo Kim <kimdwkimdw@gmail.com>
2014-10-16 21:22:00 +0900

    string, dictionary, array literals

---
+++ TheSwiftProgrammingLanguage.playground/section-1.swift
@@ -0,0 +1,32 @@
+//
+//  TheSwiftProgrammingLanguage.swift
+//
+//
+//  Created by Arkind on 2014. 10. 16..
+//
+//  https://developer.apple.com/swift/resources/
+//
+
+
+// start from https://developer.apple.com/library/ios/documentation/General/Reference/SwiftStandardLibraryReference/
+// https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/BasicOperators.html
+
+//String
+let emptyString = String()
+
+//Dictionary
+var dict = ["a":1]
+dict = [:]
+var namesOfIntegers = [Int: String]()
+var dict2:[Int: String] = [:]
+
+//Range
+4...5
+
+//
+
+
+var array = ["a","b"]
+var shoppingList: [String] = ["Eggs", "Milk"]
+
+
